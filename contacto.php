46bf4434c20db5beb39c913187271b37c3cee21a
angel03 <aviladiazjoseangel04@gmail.com>
2022-03-14 22:29:02 -0600

    Primer ScreenShot del proyecto gitmundo "angel03"

---
+++ contacto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>contactos</h1>
+    <p>
+     Gmail:aviladiazjoseangel04@gamil.com <br>
+     Telefono: 9191501670 <br>
+     Facebook: angel <br>
+     Instagram: AngelAv <br>
+    </p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<h1>Ejemplo</h1>
+<p>
+En el siguiente trabajo  trataremos sobre git que es un  sistema de control de versiones de nuestro codigo donde  pude contribuir una o mas personas.
+
+
+</p>
+
+
+</body>
+</html>
